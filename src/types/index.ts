2211ca28cdc23af71b274dfb0f3fdb99f31b8215
jaschahuisman <jascha.huisman@gmail.com>
2023-04-06 10:53:09 +0200

    updated types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,304 +1,108 @@
-import { AxiosResponse } from "axios";
-import { ControlNetUnit } from "../lib/ControlNetUnit";
 import { type Sharp } from "sharp";
+import ControlNetUnit from "../utils/controlnet/ControlNetUnit";
 
-export type SamplerName =
-  | "Euler a"
-  | "Euler"
-  | "LMS"
-  | "Heun"
-  | "DPM2"
-  | "DPM2 a"
-  | "DPM++ 2S a"
-  | "DPM++ 2M"
-  | "DPM++ SDE"
-  | "DPM fast"
-  | "DPM adaptive"
-  | "LMS Karras"
-  | "DPM2 Karras"
-  | "DPM2 a Karras"
-  | "DPM++ 2S a Karras"
-  | "DPM++ 2M Karras"
-  | "DPM++ SDE Karras"
-  | "DDIM"
-  | "PLMS"
-  | "UniPC"
-  | string;
-
-export type StableDiffusionApiConfig = {
-  host?: string;
-  port?: number | null;
-  baseUrl?: string | null;
-  timeout?: number;
-  defaultSampler?: SamplerName;
-  defaultStepCount?: number;
-  protocol?: "http" | "https";
+export type DefaultOptions = {
+  steps: number; // steps
+  cfgScale: number; // cfg_scale
+  samplerName: string; // sampler_name
+  width: number; // width
+  height: number; // height
+  batchSize: number; // batch_size
+  nIter: number; // n_iter
 };
 
-export type ApiRawResponse = {
-  image?: string;
-  images?: string[];
-  info?: any;
-  html_info?: any;
-  parameters?: any;
+export type BaseProps = {
+  prompt?: string; // prompt
+  negativePrompt?: string; // negative_prompt
+  steps?: number; // steps
+  cfgScale?: number; // cfg_scale
+  seed?: number; // seed
+  subSeed?: number; // subseed
+  subSeedStrength?: number; // subseed_strength
+  seedResizeFromHeight?: number; // seed_resize_from_h
+  seedResizeFromWidth?: number; // seed_resize_from_w
+  batchSize?: number; // batch_size
+  nIter?: number; // n_iter
+  width?: number; // width
+  height?: number; // height
+  samplerName?: string; // sampler_name
+  restoreFaces?: boolean; // restore_faces
+  sendImages?: boolean; // send_images
+  saveImages?: boolean; // save_images
+  controlNetUnits?: ControlNetUnit[]; // controlnet_units
+  styles?: string[]; // styles
+  tiling?: boolean; // tiling
+  doNotSaveSamples?: boolean; // do_not_save_samples
+  doNotSaveGrid?: boolean; // do_not_save_grid
+  eta?: number; // eta
+  sChurn?: number; // s_churn
+  sTmax?: number; // s_tmax
+  sTmin?: number; // s_tmin
+  sNoise?: number; // s_noise
+  overrideSettings?: Record<string, unknown>; // override_settings
+  overrideSettingsRestoreAfterwards?: boolean; // override_settings_restore_afterwards
+  scriptName?: string; // script_name
+  scriptArgs?: unknown[]; // script_args
+  alwaysOnScripts?: Record<string, unknown>; // alwayson_scripts
 };
 
-export type AxiosApiRawResponse = AxiosResponse<ApiRawResponse>;
-
-export type UpscalerName =
-  | "None"
-  | "Lanczos"
-  | "Nearest"
-  | "LDSR"
-  | "BSRGAN"
-  | "ESRGAN_4x"
-  | "R-ESRGAN General 4xV3"
-  | "ScuNET GAN"
-  | "ScuNET PSNR"
-  | "SwinIR 4x"
-  | string;
-
-export type HiResUpscalerName =
-  | "None"
-  | "Latent"
-  | "Latent (antialiased)"
-  | "Latent (bicubic)"
-  | "Latent (bicubic antialiased)"
-  | "Latent (nearist)"
-  | "Latent (nearist-exact)"
-  | "Lanczos"
-  | "Nearest"
-  | "ESRGAN_4x"
-  | "LDSR"
-  | "ScuNET GAN"
-  | "ScuNET PSNR"
-  | "SwinIR 4x"
-  | string;
-
-export type Txt2ImgOptions = {
-  enable_hr?: boolean;
-  hr_scale?: number;
-  hr_upscaler?: HiResUpscalerName;
-  hr_second_pass_steps?: number;
-  hr_resize_x?: number;
-  hr_resize_y?: number;
-  denoising_strength?: number;
-  firstphase_width?: number;
-  firstphase_height?: number;
-  prompt?: string;
-  styles?: string[];
-  seed?: number;
-  subseed?: number;
-  subseed_strength?: number;
-  seed_resize_from_h?: number;
-  seed_resize_from_w?: number;
-  batch_size?: number;
-  n_iter?: number;
-  steps?: number;
-  cfg_scale?: number;
-  width?: number;
-  height?: number;
-  restore_faces?: boolean;
-  tiling?: boolean;
-  do_not_save_samples?: boolean;
-  do_not_save_grid?: boolean;
-  negative_prompt?: string;
-  eta?: number;
-  s_churn?: number;
-  s_tmax?: number;
-  s_tmin?: number;
-  s_noise?: number;
-  override_settings?: Record<string, unknown>;
-  override_settings_restore_afterwards?: boolean;
-  script_args?: unknown[];
-  script_name?: string;
-  send_images?: boolean;
-  save_images?: boolean;
-  alwayson_scripts?: Record<string, unknown>;
-  controlnet_units?: ControlNetUnit[];
-  sampler_name?: SamplerName;
-  use_deprecated_controlnet?: boolean;
+export type HiResProps = {
+  enable?: boolean; // enable_hr
+  scale?: number; // hr_scale
+  upscaler?: string; // hr_upscaler
+  secondPassSteps?: number; // hr_second_pass_steps
+  resizeX?: number; // hr_resize_x
+  resizeY?: number; // hr_resize_y
+  firstPhaseWidth?: number; // firstphase_width
+  firstPhaseHeight?: number; // firstphase_height
+  denoisingStrength?: number; // denoising_strength
 };
 
-export type Img2ImgOptions = {
-  init_images: Sharp[];
-  resize_mode?: number;
-  denoising_strength?: number;
-  image_cfg_scale?: number;
-  mask_image?: any;
-  mask_blur?: number;
-  inpainting_fill?: number;
-  inpaint_full_res?: number;
-  inpaint_full_res_padding?: number;
-  inpainting_mask_invert?: number;
-  initial_noise_multiplier?: number;
-  prompt?: string;
-  styles?: [];
-  seed?: number;
-  subseed?: number;
-  subseed_strength?: number;
-  seed_resize_from_h?: number;
-  seed_resize_from_w?: number;
-  sampler_name?: SamplerName;
-  batch_size?: number;
-  n_iter?: number;
-  steps?: number;
-  cfg_scale?: number;
-  width?: number;
-  height?: number;
-  restore_faces?: boolean;
-  tiling?: boolean;
-  do_not_save_samples?: boolean;
-  do_not_save_grid?: boolean;
-  negative_prompt?: string;
-  eta?: number;
-  s_churn?: number;
-  s_tmax?: number;
-  s_tmin?: number;
-  s_noise?: number;
-  override_settings?: {};
-  override_settings_restore_afterwards?: boolean;
-  script_args?: [];
-  include_init_images?: boolean;
-  script_name?: string;
-  send_images?: boolean;
-  save_images?: boolean;
-  alwayson_scripts?: {};
-  controlnet_units?: ControlNetUnit[];
-  use_deprecated_controlnet?: boolean;
+export type Txt2ImgProps = BaseProps & {
+  hiRes: HiResProps;
 };
 
-export type ExtraBaseOptions = {
-  image: Sharp;
-  resize_mode?: number;
-  show_extras_results?: boolean;
-  gfpgan_visibility?: number;
-  codeformer_weight?: number;
-  upscaling_resize?: number;
-  upscaling_resize_w?: number;
-  upscaling_resize_h?: number;
-  upscaling_resize_crop?: boolean;
-  upscaler_1?: UpscalerName;
-  upscaler_2?: UpscalerName;
-  extras_upscaler_2_visibility?: number;
-  upscale_first?: boolean;
+export type Img2ImgProps = BaseProps & {
+  image: Sharp | Sharp[]; // init_images
+  resizeMode?: number; // resize_mode
+  imageCfgScale?: number; // image_cfg_scale
+  maskImage?: Sharp; // mask_image
+  maskBlur?: number; // mask_blur
+  inpaintingFill?: number; // inpainting_fill
+  inpaintingFullRes?: boolean; // inpainting_full_res
+  inpaintingFullResPadding?: number; // inpainting_full_res_padding
+  inpaintingMaskInvert?: boolean; // inpainting_mask_invert
+  initialNoiseMultiplier?: number; // initial_noise_multiplier
+  includeInitImages?: boolean; // include_init_images
 };
 
-export type ExtraSingleOptions = {
-  image: Sharp;
-} & ExtraBaseOptions;
+export type ExtrasProps = {};
 
-export type ExtraBatchOptions = {
-  images: Sharp[];
-  name_list: string[];
-} & ExtraBaseOptions;
-
-type ControlNetModule =
-  | "none"
-  | "canny"
-  | "depth"
-  | "depth_leres"
-  | "hed"
-  | "mlsd"
-  | "normal_map"
-  | "openpose"
-  | "openpose_hand"
-  | "clip_vision"
-  | "color"
-  | "pidinet"
-  | "scribble"
-  | "fake_scribble"
-  | "segmentation"
-  | "binary"
-  | string;
-
-export type ResizeMode = "Scale to Fit (Inner Fit)";
-
-export type Progress = {
-  progress: number;
-  eta_relative: number;
-  state: {
-    skipped: boolean;
-    interrupted: boolean;
-    job: string;
-    job_count: number;
-    job_timestamp: string;
-    job_no: number;
-    sampling_step: number;
-    sampling_steps: number;
-  };
-  current_image: string;
-  textinfo: string;
+export type ControlNetDetectProps = {
+  module?: string; // controlnet_module
+  image: Sharp | Sharp[]; // controlnet_input_images
+  processorResolution?: number; // controlnet_processor_res
+  thresholdA?: number; // controlnet_threshold_a
+  thresholdB?: number; // controlnet_threshold_b
 };
 
-export type Sampler = {
-  name: string;
-  aliases: string[];
-  options: Record<string, unknown>;
+export type ControlNetUnitProps = {
+  image?: Sharp; // input_image
+  mask?: Sharp; // mask
+  module?: string; // module
+  model?: string; // model
+  weight?: number; // weight
+  resizeMode?: string; // resize_mode
+  lowVRam?: boolean; // lowvram
+  processorResolution?: number; // processor_res
+  thresholdA?: number; // threshold_a
+  thresholdB?: number; // threshold_b
+  guidanceScale?: number; // guidance
+  guidanceStart?: number; // guidance_start
+  guidanceEnd?: number; // guidance_end
+  guessMode?: boolean; // guessmode
 };
 
-export type Upscaler = {
-  name: string;
-  model_name: string;
-  model_path: string;
-  model_url: string;
-  scale: number;
-};
+export type SdResultInfo = {};
 
-export type StableDiffusionModel = {
-  title: string;
-  model_name: string;
-  hash: string;
-  sha256: string;
-  filename: string;
-  config: string;
-};
-
-export type HyperNetwork = {
-  name: string;
-  path: string;
-};
-
-export type FaceRestorer = {
-  name: string;
-  cmd_dir: string;
-};
-
-export type RealESRGanModel = {
-  name: string;
-  path: string;
-  scale: number;
-};
-
-export type PromptStyle = {
-  name: string;
-  prompt: string;
-  negative_prompt: string;
-};
-
-export type ControlNetUnitConfig = {
-  input_image: Sharp;
-  mask?: Sharp;
-  module: ControlNetModule;
-  model?: string;
-  weight?: number;
-  resize_mode?: ResizeMode;
-  lowvram?: boolean;
-  processor_res?: number;
-  threshold_a?: number;
-  threshold_b?: number;
-  guidance?: number;
-  guidance_start?: number;
-  guidance_end?: number;
-  guessmode?: boolean;
-};
-
-export type ControlNetDetectOptions = {
-  controlnet_module?: ControlNetModule | string;
-  controlnet_input_images: Sharp[];
-  controlnet_processor_res?: number;
-  controlnet_threshold_a?: number;
-  controlnet_threshold_b?: number;
-};
-
-export type ControlNetTxt2ImgOptions = {};
+export type SdResultParameters = {};
